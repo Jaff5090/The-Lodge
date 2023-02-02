1c836ee70345c045e0161769af72d0b2a327e508
jaafar <jaafar.benhssan@mytourlive.com>
2023-02-02 14:38:20 +0100

    pass data into screens , spinner , slider , bottom navigations

--- app/src/main/java/com/example/exercice3kotlin/MainActivity.kt
+++ app/src/main/java/com/example/exercice3kotlin/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -13,14 +14,19 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var birthdateEditText: EditText
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.Btnnext)
-        val intent = Intent(this, SecondActivity::class.java)
         val datePickerButton = findViewById<ImageButton>(R.id.datePickerButton)
+
+        nameEditText = findViewById(R.id.inputName)
+        emailEditText = findViewById(R.id.inputEmail)
+        birthdateEditText = findViewById(R.id.birthday)
 
 
 
@@ -29,14 +35,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         button.setOnClickListener {
-            // Code à exécuter lorsque le bouton est cliqué
+            // execute code when bottom is clicked
+            val name = nameEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val birthdate = birthdateEditText.text.toString()
+            SendDataToActivity(name, email, birthdate)
             Toast.makeText(this, "Le bouton a été cliqué!", Toast.LENGTH_SHORT).show()
-            startActivity(intent)
+
         }
 
 
 
 
+
+
+    }
+    // send Information of main activity to quiz activity
+    private fun SendDataToActivity(name: String, email: String, birthdate: String) {
+        val intent = Intent(this, ResultQuizz::class.java)
+        intent.putExtra("NAME", name)
+        intent.putExtra("EMAIL", email)
+        intent.putExtra("BIRTHDATE", birthdate)
+        startActivity(intent)
     }
 
     ////
--- app/src/main/java/com/example/exercice3kotlin/SecondActivity.kt
+++ app/src/main/java/com/example/exercice3kotlin/SecondActivity.kt
@@ -2,9 +2,8 @@ package com.example.exercice3kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +12,36 @@ class SecondActivity : AppCompatActivity() {
         val Image1 = findViewById<ImageView>(R.id.first_image)
         val Image2 = findViewById<ImageView>(R.id.second_image)
         val Image3 = findViewById<ImageView>(R.id.third_image)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val flags = resources.getStringArray(R.array.flag_array)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, flags)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Modifiez la langue de l'application ici en fonction de la position sélectionnée
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
 
         var selectedImage: ImageView? = null
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        val window = window
+        val attributes = window.attributes
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                attributes.screenBrightness = progress.toFloat() / 100
+                window.attributes = attributes
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
 
         Image1.setOnClickListener {
             if (selectedImage != null) {
